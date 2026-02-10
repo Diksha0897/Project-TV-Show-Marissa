adce16874b99ad451276ad12ce6ee38b7839104d
marissamolejon <molejonmarissa@gmail.com>
2026-02-10 11:58:39 +0100

    creating content

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
 //You can edit ALL of the code here
-function setup() {
-  const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
+const episodesContainer = document.getElementById("episodes-container");
+
+function pad(number) {
+  return number.toString().padStart(2, "0");
 }
 
-function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+function createEpisodeCode(season, number) {
+  return `S${pad(season)}E${pad(number)}`;
 }
 
-window.onload = setup;
+function displayEpisodes(episodeList) {
+  episodesContainer.innerHTML = "";
+
+  episodeList.forEach((episode) => {
+    const card = document.createElement("div");
+    card.className = "episode-card";
+
+    const episodeCode = createEpisodeCode(
+      episode.season,
+      episode.number
+    );
+
+  card.innerHTML = `
+  <div class="episode-header">
+    <h2>${episode.name} - ${episodeCode}</h2>
+  </div>
+  <img src="${episode.image.medium}" alt="${episode.name}">
+  ${episode.summary}
+`;
+
+
+    episodesContainer.appendChild(card);
+  });
+}
+
+const allEpisodes = getAllEpisodes();
+displayEpisodes(allEpisodes);
